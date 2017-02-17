76b0ebe8a63cdc398c70361cd127d111662429f5
Harezlak Robert <harezlak.robert7@gmail.com>
2017-02-17 16:42:11 +0100

    lines refactor

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-window.onload = function() {
-const reqexp = /\s\s+/g;
-const textarea = document.getElementById('textarea');
-const div = document.getElementById('lead');
+window.onload = function () {
+    const reqexp = /\s\s+/g;
+    const textarea = document.getElementById('textarea');
+    const div = document.getElementById('lead');
 
 
-function count() {
-    let txt = textarea.value;
-    txt = txt.replace(reqexp, ' ');
-    let a = txt.split(' ').length
-    if(textarea.value === '') {
-        div.innerHTML = '';
+    function count() {
+        let txt = textarea.value;
+        txt = txt.replace(reqexp, ' ');
+        let a = txt.split(' ').length
+        if (textarea.value === '') {
+            div.innerHTML = '';
+        } else {
+            div.innerHTML = `Words: ${a}`;
+        }
     }
-    else { 
-        div.innerHTML = `Words: ${a}`;
-    }
-}
 
 
 
 
-textarea.addEventListener('keyup', count);
+    textarea.addEventListener('keyup', count);
 }
